b256e878684e9d74b294a8118339d9eeda2901ea
kerikir <vlasov-kerik@mail.ru>
2025-09-09 22:36:55 +0300

    bind the first and second row buttons to variables

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.simplecalculator
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +13,18 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Привязка переменных к кнопкам
+        val textViewSqrt: TextView = findViewById(R.id.btnSqrt)
+        val textViewLog: TextView = findViewById(R.id.btnLog2)
+        val textViewLn: TextView = findViewById(R.id.btnLn)
+        val textViewLeftBracket: TextView = findViewById(R.id.btnLeftBracket)
+        val textViewRightBracket: TextView = findViewById(R.id.btnRightBracket)
+
+        val textViewPow = findViewById<TextView>(R.id.btnPow)
+        val textViewClear = findViewById<TextView>(R.id.btnClear)
+        val textViewBack = findViewById<TextView>(R.id.btnBack)
+        val textViewPercent = findViewById<TextView>(R.id.btnPercent)
+        val textViewDiv = findViewById<TextView>(R.id.btnDiv)
     }
 }
